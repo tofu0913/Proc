fd91928aef3e7aa4fee33ad69510abbfc7090f4a
Cliff Chen <cfchen@futuredial.com>
2024-03-22 18:14:49 +0800

    Implement proc level

--- proc.lua
+++ proc.lua
@@ -11,24 +11,38 @@ _libs.lor.debug = false
 settings = _libs.lor.settings.load('data/settings.lua', {})
 
 local curSet = 0
+local curLv = 0
 
 windower.register_event('incoming text', function(_, text, _, _, blocked)
     if blocked or text == '' then
         return
     end
 
-    if string.find(text, settings["keyword"]) then
+    if string.find(text, settings["start"]) then
+       atcf(262, "Proc start")
+       curSet = 38
+       windower.send_command(string.format('input /equipset %s;wait 1;input //lua r autows;wait 1;input //aws on', curSet))
+       curLv = 0
+        
+    else
+        head = string.find(text, settings["head"])
         tail = string.find(text, settings["tail"])
-        if tail and tail > 0 then
-            found = string.sub(text, string.find(text, settings["head"])+3, tail-1)
-            atcf(262, "found:%s.", found)
-            if settings[found] and curSet ~= settings[found] then
+        if head and tail then
+            found = string.sub(text, head+3, tail-1)
+            if string.find(text, settings["big"]) then
+                level = 3
+            elseif string.find(text, settings["mid"]) then
+                level = 2
+            elseif string.find(text, settings["small"]) then
+                level = 1
+            end
+            atcf(262, "Proc found:%s, level:%s", found, level)
+                
+            if level > curLv and settings[found] and curSet ~= settings[found] then
                 curSet = settings[found]
-                windower.send_command(string.format('setkey ctrl down;wait 0.5;setkey %s down;wait 0.5;setkey %s up;setkey ctrl up', settings[found], settings[found]))
-                atcf(262, "found:%s, ready %s", found, curSet)
-            else -- big proc missed, just kill it
-                curSet = 0
-                windower.send_command(string.format('setkey alt down;wait 0.5;setkey 2 down;wait 0.5;setkey 2 up;setkey alt up'))
+                curLv = level
+                windower.send_command(string.format('input /equipset %s;wait 1;input //lua r autows;wait 1;input //aws rnd', settings[found]))
+                atcf(262, "Proc change set:%s", found)
             end
         end
     end
@@ -36,5 +50,7 @@ windower.register_event('incoming text', function(_, text, _, _, blocked)
 end)
 
 -- windower.register_event('load', function()
-    -- windower.send_command(string.format('setkey ctrl down;wait 0.5;setkey %s down;wait 0.5;setkey %s up;setkey ctrl up', 2, 2))
+       -- windower.send_command(string.format('input /equipset 39;wait 1;input //lua r autows;wait 1;input //aws on'))
 -- end)
+
+
