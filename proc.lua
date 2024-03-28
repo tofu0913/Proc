92da50d3a7d90e15a5ee19ee56f71e372ecfb04a
Cliff Chen <cfchen@futuredial.com>
2024-03-28 12:57:13 +0800

    Rewrite

--- proc.lua
+++ proc.lua
@@ -1,6 +1,6 @@
 _addon.name = 'Proc'
 _addon.author = 'Cliff'
-_addon.version = '0.0.1'
+_addon.version = '1.0.0'
 
 require('chat')
 require('logger')
@@ -11,26 +11,55 @@ _libs.lor.req('all')
 _libs.lor.debug = false
 settings = _libs.lor.settings.load('data/settings.lua', {})
 
-local curSet = 0
-local curLv = 0
-local fumeCheck = 0
-local killmode = 0
+local enable = 0
+local autowsDelay = 2
 
-local function check_fume()
-    if killmode == 1 then
-        return
+local mode = nil -- nil(waiting), kill, procing
+local confirming = nil
+local queue = {}
+local blist = {}
+
+local function parseProc(text, tail)
+    head = string.find(text, settings["head"])
+    if head and tail then
+        return string.sub(text, head+3, tail-1)
     end
-    fumeCheck = fumeCheck +1
-    if (curLv == 0 and fumeCheck > 40) or 
-       (curLv < 3 and fumeCheck > 80) or 
-        (curLv ==3 and fumeCheck> 200) then
-        windower.send_command('input /fume')
-        curSet = 38
-        windower.send_command(string.format('input /equipset %s;wait 1;input //lua r autows;wait 1;input //aws on', curSet))
-        curLv = 0
-        fumeCheck = 0
+    return nil
+end
+
+local function parseLevel(text, tail)
+    level = 0
+    if string.find(text, settings["big"]) then
+        level = 3
+    elseif string.find(text, settings["mid"]) then
+        level = 2
+    elseif string.find(text, settings["small"]) then
+        level = 1
     end
-    coroutine.schedule(check_fume, 2)
+    return level
+end
+
+local function actionToItem(action, typ, confirm)
+    items = {}
+    for k, v in pairs(action["action"]) do
+        item = {}
+        if action["eqset"] ~= nil then
+            item["eqset"] = action["eqset"]
+        end
+        item["confirm"] = confirm
+        item["action"] = v
+        item["type"] = typ
+        items[#items+1] = item
+    end
+    return items
+end
+
+local function mergeTable(t1, t2)
+    local t3 = {unpack(t1)}
+    for I = 1,#t2 do
+        t3[#t1+I] = t2[I]
+    end
+    return t3
 end
 
 windower.register_event('incoming text', function(_, text, _, _, blocked)
@@ -38,54 +67,57 @@ windower.register_event('incoming text', function(_, text, _, _, blocked)
         return
     end
 
+    if confirming and string.find(text, confirming, 1, true) ~= nil then
+        if string.find(text, settings["miss"]) == nil then
+            atcf(262, "Proc confirmed, %s, q:%s":format(confirming ,#queue))
+            confirming = nil
+            table.remove(queue, 1)
+            if mode ~= "kill" and #queue == 0 then
+                mode = nil
+            end
+        end
+    end
+
     if string.find(text, settings["start"]) then
-       atcf(262, "Proc start")
-       curSet = 38
-       windower.send_command(string.format('input /equipset %s;wait 1;input //lua r autows;wait 1;input //aws on', curSet))
-       curLv = 0
-       fumeCheck = 0
-        killmode = 0
-       -- coroutine.schedule(check_fume, 2)
+        atcf(262, "Proc start")
+        mode = nil
+        blist = {}
+        queue = {}
         
     elseif string.find(text, settings["proced"]) then
-       atcf(262, "Proc gotcha!")
-       curLv = 0
-       curSet = 0
-       fumeCheck = 0
-       windower.send_command(string.format('input /equipset %s;wait 1;input //lua r autows;wait 1;input //aws on', 38))
+        atcf(262, "Proc gotcha!")
+        if mode ~= "kill" then 
+            mode = nil
+        end
+        blist = {}
+        queue = {}
         
     elseif string.find(text, settings["max"]) or string.find(text, settings["max2"]) then
         atcf(262, "Proc kill mode")
-        windower.send_command(string.format('input /equipset 39;wait 1;input //lua r autows;wait 1;input //aws on'))
-        killmode = 1
+        mode = "kill"
 
     else
-        if killmode == 1 then
+        if mode == "kill" then
             return
         end
-        head = string.find(text, settings["head"])
-        tail = string.find(text, settings["tail"])
-        if head and tail then
-            found = string.sub(text, head+3, tail-1)
-            level = 0
-            if string.find(text, settings["big"]) then
-                level = 3
-            elseif string.find(text, settings["mid"]) then
-                level = 2
-            elseif string.find(text, settings["small"]) then
-                level = 1
-            end
-            atcf(262, "Proc found:%s, level:%s", found, level)
-                
-            if level > curLv and settings[found] and curSet ~= settings[found] then
-                curSet = settings[found]
-                curLv = level
-                fumeCheck = 0
-                windower.send_command(string.format('input /equipset %s;wait 1;input //lua r autows;wait 1;input //aws rnd', settings[found]))
-                atcf(262, "Proc target:%s, level:%s", found, level)
-            -- elseif level == curLv and curSet == settings[found] then  -- Set again
-                -- windower.send_command(string.format('input /equipset %s;wait 1;input //lua r autows;wait 1;input //aws rnd', settings[found]))
-                -- atcf(262, "Proc target again:%s, level:%s", found, level)
+        for k, v in pairs(settings["procs"]) do
+            kw = string.find(text, v["kw"])
+            if kw then
+                typ = parseProc(text, kw)
+                if typ then
+                    level = parseLevel(text)
+                    if v["actions"][typ] and not blist[typ] then
+                        mode = "procing"
+                        if level == 3 or k ~= "ws" then
+                            queue = mergeTable(actionToItem(v["actions"][typ], k, v["confirm"]), queue)
+                            atcf(262, "Proc insert")
+                        else
+                            queue = mergeTable(queue, actionToItem(v["actions"][typ], k, v["confirm"]))
+                        end
+                        blist[typ] = {}
+                        atcf(262, "Proc typ:%s, q:%s", typ ,#queue)
+                    end
+                end
             end
         end
     end
@@ -93,8 +125,75 @@ windower.register_event('incoming text', function(_, text, _, _, blocked)
 end)
 
 
--- windower.register_event('load', function()
-    -- coroutine.schedule(check_fume, 2)
--- end)
+local function launchProc(item)
 
+    if item["type"] == "ws" then
+        confirming = item["action"] .. item["confirm"]
+        -- atcf(262, ('input /ws %s <t>, waiting %s'):format(item["action"], confirming))
+        windower.send_command(('input /ws %s <t>'):format(item["action"]))
+        
+    elseif item["type"] == "bma" then
+        confirming = item["action"] .. item["confirm"]
+        -- atcf(262, ('input /ma %s <t>, waiting %s'):format(item["action"], confirming))
+        windower.send_command(('input /ma %s <t>'):format(item["action"]))
+
+    elseif item["type"] == "ja" then
+        confirming = item["action"]
+        -- atcf(262, ('input /ma %s <t>, waiting %s'):format(item["action"], confirming))
+        windower.send_command(('input /ja %s <t>'):format(item["action"]))
+    end
+end
+
+local function makesureEq(item)
+    local items = windower.ffxi.get_items()
+    local i,bag = items.equipment.main, items.equipment.main_bag
+    skill = res.skills[res.items[items['wardrobe2'][i].id].skill].en
+    if item['en'] ~= skill then
+        windower.send_command(string.format('input /equipset %s;', item['eqset']))
+        return 0
+    end
+    return 1
+end
+
+windower.register_event('prerender', function()
+    if enable == 1 then
+		local now = os.clock()
+        if (now - autowsLastCheck) >= autowsDelay then
+			local player = windower.ffxi.get_player()
+			if (player ~= nil) and (player.status == 1) then
+                if mode == "kill" or mode == nil then
+                    item = settings['killmode']
+                    if makesureEq(item) and player.vitals.tp > 999 then
+                        launchProc(item)
+                    end
+                else
+                    if #queue>0 then
+                        item = queue[1]
+                        if item["type"] == "ws" then
+                            if player.vitals.tp > 999 then
+                                if makesureEq(item) then
+                                    launchProc(item)
+                                end
+                            end
+                        elseif item["type"] == "bma" then
+                            launchProc(item)
+                        elseif item["type"] == "ja" then
+                            launchProc(item)
+                        end
+                    end
+                end
+            end
+			autowsLastCheck = now
+        end
+    end
+end)
+
+windower.register_event('load', function()
+    if not _libs.lor then
+        windower.add_to_chat(39,'ERROR: .../Windower/addons/libs/lor/ not found! Please download: https://github.com/lorand-ffxi/lor_libs')
+    end
+    atcf(262, 'Proc loaded')
+    autowsLastCheck = os.clock()
+    enable = 1
+end)
 
